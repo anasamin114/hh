9ec0bf152565e78c3dbf285a326961b3d2c598a9
anasamin114 <ellsworthalt@gmail.com>
2025-11-09 02:30:48 +0600

    Create .sql

---
+++ .sql
@@ -0,0 +1,12 @@
+sql
+CREATE DATABASE stolen_data_db;
+
+USE stolen_data_db;
+
+CREATE TABLE stolen_data (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
